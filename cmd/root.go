c747d71b4244fd4473086566056fc43bcff475e3
andscoop <andycooper.s@gmail.comm>
2021-12-30 09:50:54 -0800

    Find from stdin

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ Copyright © 2021 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -20,35 +21,41 @@ func init() {
 
 var rootCmd = &cobra.Command{
 	Use:   "cbm",
-	Short: "Aliased to 'ls'",
-	Run:   lsCmd,
+	Short: "Reads reads input from stdin and treats it like find",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("try --help")
+	},
 }
 
 var ListCmd = &cobra.Command{
 	Use:   "ls",
 	Short: "Output bookmarks in flattened list, suitable for fzf",
-	Run:   lsCmd,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := cbm.FlatList(path)
+		if err != nil {
+			panic(err)
+		}
+	},
 }
 
 var path string = "/Users/andrew.cooper/Library/Application Support/Google/Chrome/Default/Bookmarks"
 
-func lsCmd(cmd *cobra.Command, args []string) {
-	err := cbm.FlatList(path)
-	if err != nil {
-		panic(err)
-	}
-}
-
 var FindCmd = &cobra.Command{
 	Use:   "find",
 	Short: "Find the url associated with bookmark id",
-	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		var id string
-		parts := strings.Split(args[0], ":")
-		id = parts[0]
+		var parts []string
 
-		url, err := cbm.Find(path, id)
+		if len(args) > 0 {
+			parts = strings.Split(args[0], ":")
+		} else {
+			scanner := bufio.NewScanner(os.Stdin)
+			if scanner.Scan() {
+				parts = strings.Split(scanner.Text(), ":")
+			}
+		}
+
+		url, err := cbm.Find(path, parts[0])
 		if err != nil {
 			panic(err)
 		}
